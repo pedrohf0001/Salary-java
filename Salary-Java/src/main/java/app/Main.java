6bd6b744b6b929436592a8305e152cb9e73ee658
Pedro Fonseca <125426081+pedrohf0001@users.noreply.github.com>
2025-09-03 12:02:28 -0300

    Primeiro Commit desse Prj

---
+++ Salary-Java/src/main/java/app/Main.java
@@ -0,0 +1,32 @@
+package app;
+
+import entities.Employee;
+import services.PensionService;
+import services.SalaryService;
+import services.TaxService;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(("Nome: "));
+        String name= sc.nextLine();
+        System.out.println("Salário Bruto: ");
+        double grossSalary = sc.nextDouble();
+
+        Employee employee = new Employee(name, grossSalary);
+        SalaryService salaryService = new SalaryService();
+
+        double netSalary = salaryService.netSalary(employee);
+        System.out.printf("O salário liquido é = %.2f%n", netSalary);
+
+        sc.close();
+
+
+    }
+}
